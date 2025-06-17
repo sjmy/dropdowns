45f8f6f793a21da32f201caab1aa94afa62c716f
sjmy <23250749+sjmy@users.noreply.github.com>
2025-06-17 12:36:45 -0700

    Changed webpack-common entrypoint to main.js

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/js-dropdowns.js",
+  entry: "./src/main.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
