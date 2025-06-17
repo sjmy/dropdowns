595f3091900e94e3d3c7f67800cca1e1b01b95e8
sjmy <23250749+sjmy@users.noreply.github.com>
2025-06-17 16:09:54 -0700

    js-dropdowns.js gathers all dropdowns, adds click listeners, shows and hides dropdown content as needed.

--- src/js-dropdowns.js
+++ src/js-dropdowns.js
@@ -1,24 +1,26 @@
+export default function jsDropdowns() {
+  function show(dropdownContent) {
+    dropdownContent.classList.add("show");
+  }
 
+  function hide(dropdownContent) {
+    dropdownContent.classList.remove("show");
+  }
 
+  function addListeners() {
+    const dropdownButtons = document.querySelectorAll(".dropdown-button");
+    const dropdownContent = document.querySelectorAll(".dropdown-content");
 
+    for (let i = 0; i < dropdownButtons.length; i++) {
+      dropdownButtons[i].addEventListener("click", (e) => {
+        if (dropdownContent[i].classList.contains("show")) {
+          hide(dropdownContent[i]);
+        } else {
+          show(dropdownContent[i]);
+        }
+      });
+    }
+  }
 
-
-// /* When the user clicks on the button,
-// toggle between hiding and showing the dropdown content */
-// function myFunction() {
-//   document.getElementById("myDropdown").classList.toggle("show");
-// }
-
-// // Close the dropdown menu if the user clicks outside of it
-// window.onclick = function(event) {
-//   if (!event.target.matches('.dropbtn')) {
-//     var dropdowns = document.getElementsByClassName("dropdown-content");
-//     var i;
-//     for (i = 0; i < dropdowns.length; i++) {
-//       var openDropdown = dropdowns[i];
-//       if (openDropdown.classList.contains('show')) {
-//         openDropdown.classList.remove('show');
-//       }
-//     }
-//   }
-// }
+  return { addListeners };
+}
--- src/main.js
+++ src/main.js
@@ -1,1 +1,6 @@
 import "./styles.css";
+import jsDropdowns from "./js-dropdowns";
+
+const dropdowns = jsDropdowns();
+
+dropdowns.addListeners();
